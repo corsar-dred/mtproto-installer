a416de23f84ae920c1f3548510ae80f2a2b6b4e4
corsar-dred <zeus106@mail.ru>
2026-07-14 17:08:09 +0300

    Create mtproto-installer.sh

---
+++ mtproto-installer.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# MTProto Installer
+#
+# Version : 0.1.0
+# Author  : corsar-dred
+# License : MIT
+#
+###############################################################################
+
+VERSION="0.1.0"
+
+GREEN="\e[32m"
+RED="\e[31m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+CYAN="\e[36m"
+WHITE="\e[97m"
+RESET="\e[0m"
+
+clear
+
+echo -e "${CYAN}"
+echo "====================================================="
+echo "             MTProto Installer"
+echo "====================================================="
+echo -e "${RESET}"
+
+echo "Version : $VERSION"
+echo
+
+read -rp "Press ENTER to continue..."
